d4a99bc0f2abb2634985ba40219767b5d197119e
Srijan <145964130+Srijan4920@users.noreply.github.com>
2025-05-30 15:22:16 +0530

    Delete tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++
@@ -1,4 +0,0 @@
-monthly_avg_rainfall = df.groupby('Month')['Rainfall'].mean()
-highest_rainfall_month = monthly_avg_rainfall.idxmax()
-lowest_rainfall_month = monthly_avg_rainfall.idxmin()
-print(f'Highest rainfall month: {highest_rainfall_month}, Lowest rainfall month: {lowest_rainfall_month}')
